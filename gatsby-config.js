9df5ebf6bf85d4119b7bd67d50044e68ef2c6f77
Rafaell Lycan <sonny.webdsg@gmail.com>
2019-02-06 11:38:26 +0100

    Add dotenv to load configuration vars

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,16 @@
+require('dotenv').config()
+
 module.exports = {
   siteMetadata: {
     title: 'Cabare Club',
     siteUrl: 'https://cabareclub.com.br',
-    launchDate: process.env.APP_LAUNCH_DATE || '2019-6-4',
+    launchDate: process.env.APP_LAUNCH_DATE,
   },
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
-    // 'gatsby-plugin-purify-css',
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
-    // 'gatsby-plugin-react-next',
     'gatsby-plugin-lodash',
     'gatsby-plugin-catch-links',
     'gatsby-plugin-sitemap',
@@ -22,28 +22,6 @@ module.exports = {
       },
     },
     'gatsby-transformer-remark',
-    // {
-    //   resolve: 'gatsby-transformer-remark',
-    //   options: {
-    //     plugins: [
-    //       {
-    //         resolve: 'gatsby-remark-images',
-    //         options: {
-    //           maxWidth: 960,
-    //           linkImagesToOriginal: false,
-    //         },
-    //       },
-    //       {
-    //         resolve: 'gatsby-remark-responsive-iframe',
-    //         options: {
-    //           wrapperStyle: 'margin-bottom: 2rem',
-    //         },
-    //       },
-    //       'gatsby-remark-copy-linked-files',
-    //       'gatsby-remark-smartypants',
-    //     ],
-    //   },
-    // },
     {
       resolve: 'gatsby-plugin-google-fonts',
       options: {
@@ -69,24 +47,6 @@ module.exports = {
         siteUrl: `https://www.example.com`,
       },
     },
-    // {
-    //   resolve: 'gatsby-plugin-favicon',
-    //   options: {
-    //     logo: './src/assets/images/logo.png',
-    //     injectHTML: true,
-    //     icons: {
-    //       android: true,
-    //       appleIcon: true,
-    //       appleStartup: false,
-    //       coast: false,
-    //       favicons: true,
-    //       firefox: true,
-    //       twitter: false,
-    //       yandex: false,
-    //       windows: false,
-    //     }
-    //   }
-    // },
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
@@ -100,7 +60,5 @@ module.exports = {
         icon: './src/assets/images/logo.png', // This path is relative to the root of the site.
       },
     },
-    // 'gatsby-plugin-netlify-cms',
-    // 'gatsby-plugin-netlify', // make sure to keep it last in the array
   ],
 }
